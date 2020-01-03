30a9be2fc97bdcccb7b973de9e6a93769f68b76d
BankChana <bank61707@gmail.com>
2020-01-03 16:47:16 +0700

    1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NAME: ______________
-# STUDENT ID:_____________
+# NAME:Chanathip Songchanthuek
+# STUDENT ID:620612145
--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
-
-//Write function findDistance() here
-
+double findDistance (double u,double a,double t){
+  double A=(u*t)+(0.5*a*t*t);
+  return A;
+}
 int main(){
-
-  //Calling findDistance() using test cases
-
-  return 0;
+  double u,a,t;
+	cout <<"Enter u = ";
+	cin>>u ;
+  cout <<"\nEnter a = ";
+	cin>>a ;
+  cout <<"\nEnter t = ";
+	cin>>t ;
+	cout <<"\ns = " <<findDistance(u,a,t);
+	return 0;
 }
